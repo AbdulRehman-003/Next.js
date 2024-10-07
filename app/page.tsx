c497f11a9c1f6dce5294865ad36ab8d970361cf9
AbdulRehman-003 <abdulrehmanboxer003@gmail.com>
2024-10-07 18:26:28 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1>World</h1>
+    <a href="/users">Users</a>
+    <h1 className="text-3xl font-bold underline">
+      app page
+    </h1>
+    </main>
+  )
+}
