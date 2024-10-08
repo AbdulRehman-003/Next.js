1ed33dbdd3312af0ded1db57aae5059b83cfd997
AbdulRehman-003 <abdulrehmanboxer003@gmail.com>
2024-10-08 18:31:23 +0500

    add components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "./components/navbar";
+import Carousel from "./components/carousel";
+import Footer from "./components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,8 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+      <body>
+        <Navbar />
+        <Carousel />
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 
-
 export default function Home() {
   return (
     <main>
-      <h1>World</h1>
-    <a href="/users">Users</a>
-    <h1 className="text-3xl font-bold underline">
-      app page
-    </h1>
+     
     </main>
   )
 }
