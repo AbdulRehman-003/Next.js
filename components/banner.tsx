029cf4eece4285af6e3a3f2a40cbceebfd592ebc
AbdulRehman-003 <abdulrehmanboxer003@gmail.com>
2024-10-15 18:29:30 +0500

    Add files via upload

--- components/banner.tsx
+++ components/banner.tsx
@@ -4,7 +4,19 @@ import React from 'react';
 const banner = () => {
   return (
    <>
-   <img src="https://tailwindui.com/plus/img/ecommerce-images/home-page-01-hero-full-width.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+   
+<div className="image">
+ <div className=" bg-gray-900 bg-opacity-50">
+<div className="justify-center py-72">
+<h1 className="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">New arrivals are here</h1>
+<p className="text-center mb-6 text-lg font-normal text-gray-200 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">The new arrivals have, well, newly arrived. Check out the latest options from our summer small-batch release while they're still in stock.</p>
+<div className="flex justify-center">
+<button type="button" className=" text-black bg-gray-100 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-black dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Shop New Arrivals</button>
+</div>
+</div>
+</div>
+</div>
+
    </>
   )
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,9 @@ import React from 'react'
 const footer = () => {
   return (
     <>
-    
-
-<footer className="bg-white dark:bg-gray-900">
+<footer className=" bg-white dark:bg-gray-900">
     <div className="mx-auto w-full max-w-screen-xl">
-      <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
+      <div className="grid grid-cols-2 gap-8 px-20 py-6 lg:py-8 md:grid-cols-4">
         <div>
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
             <ul className="text-gray-500 dark:text-gray-400 font-medium">
@@ -112,7 +110,6 @@ const footer = () => {
       </div>
     </div>
 </footer>
-
     </>
   )
 }
